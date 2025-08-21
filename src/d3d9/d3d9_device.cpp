a5ab96d45e2c9ce6fd81c01d4f1d8fa112434fcf
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2025-08-21 11:53:54 +0200

    [d3d9] Port DXVK 2.7 changes
    [ba88d0c](https://github.com/doitsujin/dxvk/commit/ba88d0cd1c71dcffa9352d15c9c4fc69e1e25b50) - [d3d9] Handle setting/getting NPatchMode - [[d3d9] Handling more state-related setters/getters](https://github.com/doitsujin/dxvk/pull/5153)

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -2723,6 +2723,8 @@ namespace dxvk {
 
 
   HRESULT STDMETHODCALLTYPE D3D9DeviceEx::SetClipStatus(const D3DCLIPSTATUS9* pClipStatus) {
+    D3D9DeviceLock lock = LockDevice();
+
     if (unlikely(pClipStatus == nullptr))
       return D3DERR_INVALIDCALL;
 
@@ -2733,6 +2735,8 @@ namespace dxvk {
 
 
   HRESULT STDMETHODCALLTYPE D3D9DeviceEx::GetClipStatus(D3DCLIPSTATUS9* pClipStatus) {
+    D3D9DeviceLock lock = LockDevice();
+
     if (unlikely(pClipStatus == nullptr))
       return D3DERR_INVALIDCALL;
 
@@ -2944,7 +2948,7 @@ namespace dxvk {
 
 
   HRESULT STDMETHODCALLTYPE D3D9DeviceEx::SetSoftwareVertexProcessing(BOOL bSoftware) {
-    auto lock = LockDevice();
+    D3D9DeviceLock lock = LockDevice();
 
     if (bSoftware && !CanSWVP())
       return D3DERR_INVALIDCALL;
@@ -2959,19 +2963,25 @@ namespace dxvk {
 
 
   BOOL    STDMETHODCALLTYPE D3D9DeviceEx::GetSoftwareVertexProcessing() {
-    auto lock = LockDevice();
+    D3D9DeviceLock lock = LockDevice();
 
     return m_isSWVP;
   }
 
 
   HRESULT STDMETHODCALLTYPE D3D9DeviceEx::SetNPatchMode(float nSegments) {
+    D3D9DeviceLock lock = LockDevice();
+
+    m_state.nPatchSegments = nSegments;
+
     return D3D_OK;
   }
 
 
   float   STDMETHODCALLTYPE D3D9DeviceEx::GetNPatchMode() {
-    return 0.0f;
+    D3D9DeviceLock lock = LockDevice();
+
+    return m_state.nPatchSegments;
   }
 
 
