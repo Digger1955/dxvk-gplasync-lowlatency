4198ea0e677ee1d48e816ba367401ca034c1e7a2
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2025-10-29 21:44:49 +0200

    [d3d9] Port DXVK changes
    [faae3ed](https://github.com/doitsujin/dxvk/commit/faae3ed1246b3d1daa035bc3f38507062f427fb4) - [wsi] Introduce separate methods to save and restore window state

--- src/d3d9/d3d9_swapchain.cpp
+++ src/d3d9/d3d9_swapchain.cpp
@@ -1257,6 +1257,8 @@ namespace dxvk {
     
     m_monitor = wsi::getDefaultMonitor();
 
+    wsi::saveWindowState(m_window, &m_windowState, true);
+
     if (!wsi::enterFullscreenMode(m_monitor, m_window, &m_windowState, true)) {
         Logger::err("D3D9: EnterFullscreenMode: Failed to enter fullscreen mode");
         return D3DERR_INVALIDCALL;
@@ -1279,10 +1281,11 @@ namespace dxvk {
 
     ResetWindowProc(m_window);
     
-    if (!wsi::leaveFullscreenMode(m_window, &m_windowState, false)) {
+    if (!wsi::leaveFullscreenMode(m_window, &m_windowState)) {
       Logger::err("D3D9: LeaveFullscreenMode: Failed to exit fullscreen mode");
       return D3DERR_NOTAVAILABLE;
     }
+    wsi::restoreWindowState(m_window, &m_windowState, false);
 
     m_parent->NotifyFullscreen(m_window, false);
     
