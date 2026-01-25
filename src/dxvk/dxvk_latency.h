ccd54afe677672b53f4dee34a1f97406c02c10e1
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-01-25 05:12:24 +0200

    [dxvk] Port DXVK Low Latency changes
    [b10ff87](https://github.com/netborg-afps/dxvk/commit/b10ff874cb534a8ab7236b3a7226991c883be9f4) - [dxvk] Add query pools to the frame pacer - [Use calibrated device timestamps for each submit](https://github.com/netborg-afps/dxvk/pull/9)

--- src/dxvk/dxvk_latency.h
+++ src/dxvk/dxvk_latency.h
@@ -222,6 +222,11 @@ namespace dxvk {
     virtual DxvkLatencyStats getStatistics(
             uint64_t                  frameId) = 0;
 
+    virtual VkQueryPool* allocSubmitQueryPool() { return nullptr; }
+
+    virtual void freeSubmitQueryPool(
+            VkQueryPool*              queryPool ) { }
+
   private:
 
     std::atomic<uint64_t> m_refCount = { 0u };
