d37621fbffd52e175ab3aae0a01bb28f6e3659f0
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-01-25 05:13:37 +0200

    [dxvk] Port DXVK Low Latency changes
    [28a142e](https://github.com/netborg-afps/dxvk/commit/28a142e10ff0a1e7d0885fe25d17d5fb8ea43f88) - [dxvk] Write device timestamp at the end of each submit - [Use calibrated device timestamps for each submit](https://github.com/netborg-afps/dxvk/pull/9)

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -79,7 +79,7 @@ namespace dxvk {
   }
   
   
-  Rc<DxvkCommandList> DxvkContext::endRecording(
+  std::pair<Rc<DxvkCommandList>, VkQueryPool*> DxvkContext::endRecording(
     const VkDebugUtilsLabelEXT*       reason) {
     this->endCurrentCommands();
     this->relocateQueuedResources();
@@ -91,6 +91,15 @@ namespace dxvk {
       m_descriptorPool = m_descriptorManager->getDescriptorPool();
     }
 
+    VkQueryPool* queryPool = m_latencyTracker ? m_latencyTracker->allocSubmitQueryPool() : nullptr;
+    if (queryPool) {
+      m_cmd->cmdResetQueryPool(DxvkCmdBuffer::ExecBuffer, *queryPool, 0, 1);
+      m_cmd->cmdWriteTimestamp(DxvkCmdBuffer::ExecBuffer,
+        VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+        *queryPool, 0
+      );
+    }
+
     if (unlikely(m_features.test(DxvkContextFeature::DebugUtils))) {
       // Make sure to emit the submission reason always at the very end
       if (reason && reason->pLabelName && reason->pLabelName[0])
@@ -98,7 +107,7 @@ namespace dxvk {
     }
 
     m_cmd->finalize();
-    return std::exchange(m_cmd, nullptr);
+    return std::make_pair(std::exchange(m_cmd, nullptr), queryPool);
   }
 
 
@@ -140,8 +149,9 @@ namespace dxvk {
     if (m_endLatencyTracking && m_latencyTracker)
       m_latencyTracker->notifyCsRenderEnd(m_latencyFrameId);
 
-    m_device->submitCommandList(this->endRecording(reason),
-      m_latencyTracker, m_latencyFrameId, status);
+    auto [cmdList, queryPool] = this->endRecording(reason);
+    m_device->submitCommandList(cmdList,
+      m_latencyTracker, m_latencyFrameId, queryPool, status);
 
     // Ensure that subsequent submissions do not see the tracker.
     // It is important to hide certain internal submissions in
