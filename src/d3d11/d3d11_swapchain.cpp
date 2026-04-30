49a83b7b841699e85ca313135141782ef5402563
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-04-30 12:08:34 +0200

    Update DXVK-GPLALL 2.7.1-2 to DXVK-GPLALL 2.7.1-3
    Backported build improvements from DXVK-GPLALL 2.6.6, minor updates to README.md and dxvk.conf.
    
    **List of ported and adapted DXVK Low Latency commits**: 
    
    [40e778a](https://github.com/netborg-afps/dxvk-low-latency/commit/40e778a42b914b204586ac275260f70b0798e92a) - [dxvk] Enable calibrated timestamps only for low-latency modes - [Minor optimizations](https://github.com/netborg-afps/dxvk-low-latency/pull/15)
    
    [159ff57](https://github.com/netborg-afps/dxvk-low-latency/commit/159ff57d54272e71def0a4203f3cf8a13ed41bb3) - [dxvk] Fix ringbuffer allocator bug - [Minor optimizations](https://github.com/netborg-afps/dxvk-low-latency/pull/15)
    
    [eeb9e83](https://github.com/netborg-afps/dxvk-low-latency/commit/eeb9e832c0f4c328ce65a071785fd1a81332ca27) - [dxvk] Reduce memory footprint of latency markers - [Minor optimizations](https://github.com/netborg-afps/dxvk-low-latency/pull/15)
    
    [e554b1b](https://github.com/netborg-afps/dxvk-low-latency/commit/e554b1b9b09ab524f31ce02fe99aae9f99fc7cca) - [dxvk] Optimize DxvkSharedAllocationCache::freeAllocation - [Minor optimizations](https://github.com/netborg-afps/dxvk-low-latency/pull/15)
    
    [c42092a](https://github.com/netborg-afps/dxvk-low-latency/commit/c42092abe4925a27188c9fe61a9a21b4a038bad1) - [dxvk] Optimize DxvkCsChunkPool - [Minor optimizations](https://github.com/netborg-afps/dxvk-low-latency/pull/15)
    
    [b81475c](https://github.com/netborg-afps/dxvk-low-latency/commit/b81475c3e3cc68dd4d50a2493ad6047a6d25e09e) - [dxvk] Optimize sync::Fence - [Minor optimizations](https://github.com/netborg-afps/dxvk-low-latency/pull/15)
    
    [96daf3c](https://github.com/netborg-afps/dxvk-low-latency/commit/96daf3cada90940460dcdc68d8aa5bdb29cfedfa) - [util] Add sync::AtomicSignal based on WaitOnAddress / futex - [Threaded sleep](https://github.com/netborg-afps/dxvk-low-latency/pull/16/)
    
    [5311d42](https://github.com/netborg-afps/dxvk-low-latency/commit/5311d4261a1c8188b5b66faa065ba43d77535174) - [dxvk] Add ThreadedSleep - [Threaded sleep](https://github.com/netborg-afps/dxvk-low-latency/pull/16/)
    
    [6ccd7b4](https://github.com/netborg-afps/dxvk-low-latency/commit/6ccd7b4958364c00137bf57c962fde2679c013af) - [dxvk] Add frame duration measurement for app-thread - [Jitter display](https://github.com/netborg-afps/dxvk-low-latency/pull/17)
    
    [9b85b1e](https://github.com/netborg-afps/dxvk-low-latency/commit/9b85b1e6f41664ad51fcaed31ff75605881367f5) - [dxvk] Add jitter tracking - [Jitter display](https://github.com/netborg-afps/dxvk-low-latency/pull/17)
    
    [07d5b3e](https://github.com/netborg-afps/dxvk-low-latency/commit/07d5b3e45f83c12e3fce27414ddc6fbfcfebb886) - [hud] Change initialization location of latency hud items - [Jitter display](https://github.com/netborg-afps/dxvk-low-latency/pull/17)
    
    [9659672](https://github.com/netborg-afps/dxvk-low-latency/commit/96596727c97d7df016d7d3df87b36fe25afa9532) - [hud] Add jitter hud item - [Jitter display](https://github.com/netborg-afps/dxvk-low-latency/pull/17)

--- src/d3d11/d3d11_swapchain.cpp
+++ src/d3d11/d3d11_swapchain.cpp
@@ -298,6 +298,9 @@ namespace dxvk {
     if (m_renderLatencyHud)
       m_renderLatencyHud->updateLatencyTracker(m_latency);
 
+    if (m_jitterHud)
+      m_jitterHud->updateLatencyTracker(m_latency);
+
     if (m_latencyDetailsHud)
       m_latencyDetailsHud->updateLatencyTracker(m_latency);
 
@@ -612,9 +615,12 @@ namespace dxvk {
         m_latencyHud = hud->addItem<hud::HudLatencyItem>("latency", 4);
         FramePacer* framePacer = dynamic_cast<FramePacer*>(m_latency.ptr());
         if (framePacer) {
-          int32_t fpsItemPos = hud->getItemPos<hud::HudFpsItem>();
-          m_renderLatencyHud = hud->addItem<hud::HudRenderLatencyItem>("renderlatency", fpsItemPos+1);
-          m_latencyDetailsHud = hud->addItem<hud::HudLatencyDetailsItem>("latencydetails", fpsItemPos+2);
+          m_renderLatencyHud = reinterpret_cast<hud::HudRenderLatencyItem*>(
+            hud->getItem<hud::HudRenderLatencyItem>().ptr() );
+          m_jitterHud = reinterpret_cast<hud::HudJitterItem*>(
+            hud->getItem<hud::HudJitterItem>().ptr() );
+          m_latencyDetailsHud = reinterpret_cast<hud::HudLatencyDetailsItem*>(
+            hud->getItem<hud::HudLatencyDetailsItem>().ptr() );
         }
       }
     }
